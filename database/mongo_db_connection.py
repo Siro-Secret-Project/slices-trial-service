2225249fc52ec3657c0312ba59e794ac59ca1142
GauravVyasGit <gauravyas21@gmail.com>
2025-02-07 11:13:50 +0530

    Changed MongoDB String to Prod

--- database/mongo_db_connection.py
+++ database/mongo_db_connection.py
@@ -2,5 +2,5 @@ from pymongo import MongoClient
 
 db_client = MongoClient("mongodb+srv://gauravvyas:oFQ46Uomi9XSrl6g@ssp-dev.6s6oi.mongodb.net/")
 
-db = db_client["SSP-dev"]
+db = db_client["SSP-prod"]
 
