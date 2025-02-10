fd9029a47114d5c84b90e6e4d9a6fa3ab1c006c2
Saad <saad.shirgaonkar@siroclinpharm.com>
2025-02-10 15:28:35 +0530

    done

--- document_retrieval/utils/process_filters.py
+++ document_retrieval/utils/process_filters.py
@@ -3,29 +3,32 @@ def process_filters(documents, filters):
 
     for doc in documents:
         # Check phase match (OR logic)
-        if not any(phase in filters['phases'] for phase in doc['phases']):
+        if filters['phases'] and not any(phase in filters['phases'] for phase in doc['phases']):
             continue
 
         # Check location match based on countryLogic
-        if filters['countryLogic'] == 'AND':
-            if not all(loc in doc['locations'] for loc in filters['locations']):
-                continue
-        else:  # OR logic
-            if not any(loc in doc['locations'] for loc in filters['locations']):
-                continue
+        if filters['locations']:
+            if filters['countryLogic'] == 'AND':
+                if not all(loc in doc['locations'] for loc in filters['locations']):
+                    continue
+            else:  # OR logic
+                if not any(loc in doc['locations'] for loc in filters['locations']):
+                    continue
 
         # Check sponsorType match
-        if doc['sponsorType'] != filters['sponsorType']:
+        if filters['sponsorType'] and doc['sponsorType'] != filters['sponsorType']:
             continue
 
         # Check date range
-        if not (filters['startDate'] <= doc['startDate'] <= filters['endDate'] and
-                filters['startDate'] <= doc['endDate'] <= filters['endDate']):
-            continue
+        if filters['startDate'] and filters['endDate']:
+            if not (filters['startDate'] <= doc['startDate'] <= filters['endDate'] and
+                    filters['startDate'] <= doc['endDate'] <= filters['endDate']):
+                continue
 
         # Check sample size range
-        if not (filters['sampleSizeMin'] <= doc['enrollmentCount'] <= filters['sampleSizeMax']):
-            continue
+        if filters['sampleSizeMin'] is not None and filters['sampleSizeMax'] is not None:
+            if not (filters['sampleSizeMin'] <= doc['enrollmentCount'] <= filters['sampleSizeMax']):
+                continue
 
         filtered_docs.append(doc)
 
